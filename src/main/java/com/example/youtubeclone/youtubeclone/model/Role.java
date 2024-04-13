a580af0ae9246a62a6b1813dfdc1b93da675d06e
bhavish222 <bhavish0970@chitkara.edu.in>
2024-04-13 11:06:00 +0530

    add role

---
+++ src/main/java/com/example/youtubeclone/youtubeclone/model/Role.java
@@ -0,0 +1,24 @@
+package com.example.youtubeclone.youtubeclone.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "role")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "role_id")
+    private Long roleId;
+    @Column(name = "role")
+    private String role;
+    @Column(name = "username")
+    private String username;
+}
