9fb46c3f1f11e314aa22646761bd112d15a0f3a1
bhavish222 <bhavish0970@chitkara.edu.in>
2024-04-13 13:48:55 +0530

    rest controller for channel

---
+++ src/main/java/com/example/youtubeclone/youtubeclone/controller/ChannelController.java
@@ -0,0 +1,13 @@
+package com.example.youtubeclone.youtubeclone.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@AllArgsConstructor
+public class ChannelController {
+
+//    @GetMapping("/channel/{channelId}")
+
+}
